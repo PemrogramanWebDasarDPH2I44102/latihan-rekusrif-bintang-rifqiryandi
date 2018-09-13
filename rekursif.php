9ae408c3c9e81683232a9bb9a804c9c0a51286a2
Rifqi ryandi <42662867+rifqiryandi@users.noreply.github.com>
2018-09-13 08:57:57 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,43 @@
 <?php
-
 function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+        $c = $i;
+    rek($c);
+    echo "<br>";
+    $i--;
+    if($i>0){
+        rekursif($i);
+    }
+  }
+
+  function rek($k){
+    echo "*";
+    $k--;
+    if ($k > 0) {
+        rek($k);
+        }
+    }
+
+  rekursif(5);
+
+  echo"<br> ================= <br>";
+
+$batas=1;
+function reke($y = 1){
+
+global $batas;
+echo "$y";
+$y++;
+if ($y<=$batas) {
+    reke($y);
+}else {
+    echo "<br>";
+    $batas++;
+    if ($batas<=5) {
+        reke();
+        }
+    }
 }
-rekursif(5);
+
+     
+    reke();
 ?>
